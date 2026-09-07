c3fd8b40e9ccdede315c3da0a89acddb434a92ea
Leo <www.krasavinlv@gmail.com>
2026-09-07 13:11:29 +0300

    fix:fixed env/config mismathing stable

--- backend/cmd/main.go
+++ backend/cmd/main.go
@@ -9,11 +9,28 @@ import (
 	"tm/internal/auth"
 	"tm/internal/config"
 	"tm/internal/repository/postgres"
+	"github.com/joho/godotenv"
 	"tm/internal/services"
 )
 
 func main() {
-	path := os.Getenv("CONFIG_PATH")
+	err := godotenv.Load()
+
+	if err != nil {
+		log.Println("godotenv error:", err)
+	}
+
+	config_env := ""
+	switch os.Getenv("ENV"){
+	case "local":
+		config_env = "CONFIG_PATH_BACKEND"
+	case "dev":
+		config_env = "CONFIG_PATH_DOCKER"
+	default:
+		log.Fatal("wrong work env")
+	}
+
+	path := os.Getenv(config_env)
 	cfg, erro := config.ConfigInit(path)
 	if erro != nil {
 		log.Fatalf("failed config init with error %v\n", erro)
@@ -23,7 +40,6 @@ func main() {
 	if err != nil {
 		log.Fatalf("failed connect to db with error %v \n", err)
 	}
-
 
 	jwtsecret := os.Getenv("JWT_SECRET")
 	JWTManager := auth.NewJWTManager(jwtsecret) 
